d01f0d8fdd55868498f495cb2ec66cfcc5491116
Ngô Đức Thông <65167293+thongngoduc@users.noreply.github.com>
2020-05-24 23:39:21 +0700

    Add files via upload

---
+++ b8.8.py
@@ -0,0 +1,9 @@
+from tkinter import *
+tk = Tk()
+cb=Label(tk, text="Họ và tên: ngô đức thông")
+cb.grid(column=0,row=1)
+cb=Label(tk, text="Lớp: K59dtvt")
+cb.grid(column=0,row=2)
+cb=Label(tk, text="MSS: 18575202070002")
+cb.grid(column=0,row=3)
+tk.mainloop()
